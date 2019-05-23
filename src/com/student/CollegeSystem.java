e4b7f60db2347dce841470071b576a2c5ffac722
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-23 19:08:02 -0400

    [Neeraj] displayed my name ten times in CollegeSystem

--- src/com/student/CollegeSystem.java
+++ src/com/student/CollegeSystem.java
@@ -3,6 +3,7 @@ package com.student;
 public class CollegeSystem
 {
     public static void main(String[] args){
-        System.out.println("Hello Java");
+        //System.out.println("Hello Java");
+        for (int i=0; i<10; i++) System.out.println("Neeraj");
     }
 }
