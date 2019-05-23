e0a0fd851951854c20f5acbf408fa4fa1f0bc6b0
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-23 18:58:56 -0400

    [Neeraj] added a new line in CollegeSystem

--- src/com/student/CollegeSystem.java
+++ src/com/student/CollegeSystem.java
@@ -3,6 +3,7 @@ package com.student;
 public class CollegeSystem
 {
     public static void main(String[] args){
+        System.out.println("This is my NAME");
         System.out.println("Hello Java");
     }
 }
