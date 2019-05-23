cb23de38f825e2c3c4ba630ea3356cc1f1889590
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-23 19:00:33 -0400

    [Neeraj] deleted a line in CollegeSystem

--- src/com/student/CollegeSystem.java
+++ src/com/student/CollegeSystem.java
@@ -3,7 +3,6 @@ package com.student;
 public class CollegeSystem
 {
     public static void main(String[] args){
-        System.out.println("This is my NAME");
         System.out.println("Hello Java");
     }
 }
